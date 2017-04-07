b7383d4dd76a33dad31c585a7be29b04e9b524ae
S. J. Lee <sj9401h@naver.com>
2017-04-07 18:46:55 +0900

    ver 0.2.8
    * 선택적 여러편 다운로드 기능 추가! (프로그램 내 사용법 참고)
    * 다운로더 클래스와 전처리 클래스 분리
    * Scanner 를 BufferedReader로 변경
    * List에 담기던 String형식의 주소를 Comic클래스로 재정의하여 담음.
    * Comic 클래스 내부엔 title(만화 제목), addr(아카이브주소) 스트링 변수만 존재
    * 도움말 항목 추가 (9번)
    * 기존에 있던 아카이브 최신 유지 기능 잠정 폐지(없어도 알아서 리디렉션 됨)

--- MMDownloader/src/ui/UI.java
+++ MMDownloader/src/ui/UI.java
@@ -1,8 +1,9 @@
 package ui;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-import downloader.Downloader;
+import downloader.Preprocess;
 import sys.SystemInfo;
 
 public class UI {
@@ -19,39 +20,56 @@ public class UI {
 		return instance;
 	}
 
-	public void showMenu() {
+	public void showMenu() throws Exception {
 
-		Scanner sc = new Scanner(System.in);
-		Downloader downloader = Downloader.getInstance();
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		//Scanner sc = new Scanner(System.in);
+		
+		Preprocess preprocess = Preprocess.getInstance();
 
 		int menuNum = Integer.MAX_VALUE;
 		String comicAddress;
 
 		while (menuNum != EXIT) {
-			System.out.println("메뉴를 선택하세요\n  1. 만화 다운로드\n  2. 다운로드 폴더 열기\n  3. 마루마루 접속\n  0. 종료");
-			menuNum = sc.nextInt();
+			System.out.println("메뉴를 선택하세요\n  1. 만화 다운로드\n  2. 선택적 다운로드\n  3. 다운로드 폴더 열기\n  4. 마루마루 접속\n  9. 도움말\n  0. 종료");
+			menuNum = Integer.parseInt(in.readLine());
 
 			switch (menuNum) {
 			case 1:
 				System.out.print("주소를 입력하세요: ");
-				comicAddress = sc.next().trim();
-				downloader.download(comicAddress);
-				downloader.close();
+				comicAddress = in.readLine().trim();
+				preprocess.connector(comicAddress, 0, in);
+				preprocess.close();
 				break;
 
 			case 2:
+				System.out.print("전체보기 주소를 입력하세요: ");
+				comicAddress = in.readLine().trim();
+				preprocess.connector(comicAddress, 1, in);
+				preprocess.close();
+				break;
+				
+			case 3:
 				SystemInfo.makeDir();
 				SystemInfo.openDir();
 				break;
-			case 3:
+			
+			case 4:
 				SystemInfo.openBrowser();
 				break;
+			
+			case 9:
+				SystemInfo.help();
+				break;
+			
 			case 0:
 				System.out.println("프로그램을 종료합니다");
 				break;
 			}
 		}
-		sc.close();
+		
+		in.close();
 	}
 	
 	public void close(){
