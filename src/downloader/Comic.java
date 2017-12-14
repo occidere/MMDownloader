5b09a44d2c736f3678e1b07c41b5f1b2c9f13d66
S.J. Lee <occidere@naver.com>
2017-12-14 23:32:21 +0900

    Comic.java 리팩토링 1. 생성자 String 매개변수에 trim() 적용 2. toString() Override

--- src/downloader/Comic.java
+++ src/downloader/Comic.java
@@ -6,7 +6,6 @@ import java.util.List;
  * 만화 아카이브 주소 파싱 시 "아카이브 주소"와, "주소" 등의 정보를 모아놓은 클래스.
  * 현재 최소 경량화 상태
  * @author occidere
- *
  */
 public class Comic {
 	private String title; //해당 주소와 매칭되는 제목. ex) 원피스
@@ -17,9 +16,9 @@ public class Comic {
 	public Comic(){}
 	
 	public Comic(String title, String titleNo, String address, List<String> imgUrl){
-		this.title = title;
+		this.title = title.trim();
 		this.titleNo = titleNo;
-		this.address = address;
+		this.address = address.trim();
 		this.imgURL = imgUrl;
 	}
 
@@ -54,4 +53,15 @@ public class Comic {
 	public void setImgURL(List<String> imgURL) {
 		this.imgURL = imgURL;
 	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s_%s\t%s\t%s", title, titleNo, address, imgURL);
+	}
 }
+
+/*
+변경사항:
+생성자에 trim 적용
+toString 추가
+*/
