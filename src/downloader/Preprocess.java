3056d83aa7179cddbbe8e5372dcb1d615afd472e
S.J. Lee <occidere@naver.com>
2017-12-14 23:33:00 +0900

    Preprocess.java 리팩토링 1. DCL 싱글톤 적용 2. 정규식 파싱 메서드(parse)에 Stream 적용 3. 파싱 시 각 페이지 별로 try...catch 이용하여 최대한 파싱

--- src/downloader/Preprocess.java
+++ src/downloader/Preprocess.java
@@ -3,14 +3,13 @@ package downloader;
 import java.io.BufferedReader;
 
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.LinkedList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import downloader.Downloader;
@@ -20,18 +19,25 @@ import common.ErrorHandling;
 
 public class Preprocess implements DownloadMod {
 	
+	private final Downloader downloader; //다운로더 객체
+	
 	/* 싱글톤 패턴 */
 	private Preprocess(){
 		downloader = Downloader.getInstance();
 	}
 
-	private static Preprocess instance;
+	private static volatile Preprocess instance = null;
+	
+	/* DCL Singleton */
 	public static Preprocess getInstance(){
-		if(instance==null) instance = new Preprocess();
+		if(instance == null) {
+			synchronized(Preprocess.class) {
+				if(instance == null)
+					instance = new Preprocess();
+			}
+		}
 		return instance;
 	}
-	
-	private Downloader downloader; //다운로더 객체
 
 	/**
 	 * UI에서 주소를 스트링으로 입력받아 아카이브 리스트를 만들어주는 메서드
@@ -39,7 +45,7 @@ public class Preprocess implements DownloadMod {
 	 * @param downloadMode 다운로드 모드
 	 * @param in BufferedReader 객체
 	 */
-	public void connector(String rawAddress, int downloadMode, BufferedReader in) {
+	public void connector(String rawAddress, int downloadMode, final BufferedReader in) {
 		//아카이브 주소가 담긴 리스트.
 		List<Comic> archiveAddrList = getArchiveAddrList(rawAddress);
 		
@@ -58,7 +64,6 @@ public class Preprocess implements DownloadMod {
 
 				//잘못된 정규식 삽입시 NullPointerException
 				List<Integer> pages = parse(command, archiveAddrList.size());
-				
 				downloader.selectiveDownload(archiveAddrList, pages);
 			}
 			catch(Exception e){
@@ -76,10 +81,7 @@ public class Preprocess implements DownloadMod {
 	 * @return wasabisyrup과 같은 아카이브 주소가 담긴 리스트
 	 */
 	private List<Comic> getArchiveAddrList(String rawAddress){
-		List<Comic> archiveAddrList = new ArrayList<>();
-		
-		//각 아카이브주소와 매핑되는 제목, 아카이브주소
-		String archiveTitle, archiveAddr;
+		List<Comic> archiveAddrList = new LinkedList<>();
 		
 		//wasabisyrup, yuncomics, shencomics와 같은 아카이브 주소가 들어오는 경우
 		if(rawAddress.contains("http") && rawAddress.contains("archives")){
@@ -97,24 +99,18 @@ public class Preprocess implements DownloadMod {
 			
 			/* 정규식 좀더 강력하게 수정-> <div class="Content">에서 href=".../archives/.."가 포함된 모든 주소 파싱 */
 			Elements divContent = doc.select("div.content").select("[href*=/archives/]");
-			//System.out.println(divContent);
 
-			for(Element e : divContent){
-				archiveTitle = e.text().trim();
-				/* 닥터스톤 1화, 아발트 1화의 경우 텍스트 없이 주소값만 중복으로 입력되있음.
-				 * 어차피 바로 아래에 정상적으로 있으므로 그냥 걸러줌
-				 * <a target="_self" href="http://wasabisyrup.com/archives/U2j2ASrArLA"></a>
-				 * <a target="_self" href="http://wasabisyrup.com/archives/U2j2ASrArLA">닥터 스톤(Dr. Stone) 1화</a> */
-				if(archiveTitle == null || archiveTitle.equals("")) continue;
-				
-				//archiveAddr = toNewArchivesName(e.attr("href").trim());
-				archiveAddr = e.attr("href").trim();
-				archiveAddrList.add( new Comic(archiveTitle, null, archiveAddr, null) );
-			}
+			/* e.text(): 만화 제목
+			 * e.attr("href"): 만화 아카이브 주소 */
+			archiveAddrList = divContent.stream()
+					.filter(e-> e.text().trim() != null && e.text().trim().length() > 0) //정상적인 제목을 지닌 만화들만 추출
+					.map(e-> new Comic(e.text(), null, e.attr("href"), null)) //정상 만화들의 제목, 아카이브 주소를 담은 Comic 객체로 변환
+					.collect(Collectors.toList()); //리스트로 Collect
 		}
 		catch(Exception e){
-			ErrorHandling.saveErrLog("Jsoup 파싱 실패", "", e);
+			ErrorHandling.saveErrLog("Jsoup 파싱 실패", "ArchiveAddrList 파싱 실패", e);
 		}
+		
 		return archiveAddrList;
 	}
 	
@@ -123,86 +119,64 @@ public class Preprocess implements DownloadMod {
 	 * 인덱스는 1부터 시작으로 변경
 	 * @param archiveAddrList 아카이브 주소가 담긴 리스트
 	 */
-	private void showList(List<Comic> archiveAddrList){
+	private void showList(final List<Comic> archiveAddrList){
 		int idx = 1;
-		for(Comic comic : archiveAddrList) {
-			System.out.println(String.format("%3d. %s", idx++, comic.getTitle()));
-		}
+		for(Comic comic : archiveAddrList)
+			System.out.printf("%3d. %s\n", idx++, comic.getTitle());
 	}
 	
 	/**
-	 * 여러편 다운로드를 위한 정규식 파싱해서 페이지 번호를 담은 int[] 로 반환
-	 * @param command 여러편 다운로드 정규식
+	 * 여러편 다운로드를 위한 정규식을 파싱하여 페이지 번호를 담은 {@code List<Integer>}를 반환
+	 * @param COMMAND 여러편 다운로드 정규식
+	 * @param MAX_PAGE 최대 페이지 번호 = 만화 주소 리스트의 길이
 	 * @return 페이지 번호를 담은 리스트
 	 */
-	private List<Integer> parse(String command, int maxPage){
-		String spliter = ","; //최우선 구분자
-		String delim[] = { "~", "-" }; //페이지 간 구분자
+	private List<Integer> parse(final String COMMAND, final int MAX_PAGE){
+		final String spliter = ",";				//최우선 구분자
+		final String delim[] = { "~", "-" };	//페이지 간 구분자
+		final String splitRegex = String.format("[%s|%s]", delim[0], delim[1]); //페이지 간 구분자 정규식
 		
-		//구분자를 기준으로 하여"1,2,3-4,5~6 을 { "1", "2", "3-4", "5~6" }으로 split
-		String splitted[] = command.split(spliter);
-		String strPages[]; //임시로 사용될 스트링 배열 타입의 페이지 번호 집합
-
-		Set<Integer> tmpPages = new HashSet<>();
-		List<Integer> pages = null;
+		List<Integer> pages = new LinkedList<>(); //최종 페이지 저장 리스트
 		
-		int i, j, start, end, splittedLen = splitted.length, tmp, tmpPagesLen, pageNum;
-
-		try{
-			for(i = 0; i < splittedLen; i++){
-				//delmin(~, -)을 기준으로 쪼갠다.
-				strPages = splitted[i].split(String.format("%s|%s", delim[0], delim[1]));
-				
-				//스트링 페이지 배열 길이가 1이면, 숫자 1개라는 의미
-				if(1 == strPages.length){
-					pageNum = Integer.parseInt(strPages[0]);
-					if(isCorrectPage(1, maxPage, pageNum)) tmpPages.add(pageNum);
-				}
-				//아니라면 s = 시작페이지, e = 끝페이지 까지 순차 저장.
-				else{ 
-					start = Integer.parseInt(strPages[0]);
-					end = Integer.parseInt(strPages[1]);
-					
-					//페이지 순서 오름차순 유지
-					if(end < start){
-						tmp = start;
-						start = end;
-						end = tmp;
+		try {
+			final List<Integer> tmpPages = new LinkedList<>(); //임시 페이지 저장 리스트
+			
+			//공백 제거 후, 구분자를 기준으로 하여"1,2,3-4,5~6 을 { "1", "2", "3-4", "5~6" }으로 split
+			Arrays.stream(COMMAND.replaceAll(" ", "").split(spliter)) 
+				.map(x-> x.split(splitRegex)) //범위식(~, -)이면 길이 2, 일반 페이지면 길이 1인 배열
+				.forEach(x-> {
+					try {
+						int start = Integer.parseInt(x[0]), end;
+								
+						if(x.length == 1) { //스트링 페이지 배열 길이가 1이면, 숫자 1개라는 의미
+							if(isCorrectPage(1, MAX_PAGE, start))
+								tmpPages.add(start); //정상 페이지면 저장
+						}
+						else { //범위식인 경우. ex) "3 ~ 4" -> {"3", "4"}
+							start = Math.min(Integer.parseInt(x[0]), Integer.parseInt(x[1]));
+							end = Math.max(Integer.parseInt(x[0]), Integer.parseInt(x[1]));
+							
+							IntStream.rangeClosed(start, end) // [start, end] 생성
+								.filter(y-> isCorrectPage(1, MAX_PAGE, y)) //각 숫자별로 정상페이지인지 검사
+								.forEach(tmpPages::add); //정상 페이지는 추가
+						}
 					}
-
-					for(j = start; j <= end; j++){
-						if(isCorrectPage(1, maxPage, j))
-							tmpPages.add(j);
+					catch(Exception e) {
+						ErrorHandling.saveErrLog("잘못된 페이지", "정규식: "+COMMAND, e); //로그에 커맨드 출력
 					}
-				}
-			}
+				});
+			pages = tmpPages.stream()
+					.distinct()		//중복제거
+					.map(x-> x-1)	//페이지번호 -> 인덱스
+					.sorted()		//오름차순 정렬
+					.collect(Collectors.toList());
 			
-			//임시페이지 저장소의 크기
-			tmpPagesLen = tmpPages.size();
-			
-			pages = new ArrayList<>(tmpPagesLen);
-			
-			//Set에 담긴 중복이 제거된 페이지 번호를 iterator와 auto unboxing 기능을 활용하여 배열에 저장
-			Iterator<Integer> it = tmpPages.iterator();
-			while(it.hasNext()){
-				tmp = it.next();
-				// 인덱스 번호를 1번부터 입력받도록 했으므로, -1 필수
-				if(tmp<=maxPage)  pages.add(tmp - 1);
-			}
-			
-			//오름차순 페이지 정렬
-			java.util.Collections.sort(pages);
-			
-			//페이지 번호가 담긴 배열의 크기가 0이면 에러
-			if(pages.size()==0) throw new IndexOutOfBoundsException("페이지 선택 형식이 잘못되었습니다.");
+			if(pages.size() == 0) throw new Exception("페이지 선택 형식이 잘못되었습니다.");
 		}
-		catch(IndexOutOfBoundsException iobe){
-			//어지간한 페이지 에러는 다 여기서 잡힌다.
-			ErrorHandling.printError("유효한 페이지를 입력해주세요.", false);
-		}
-		catch(Exception e){
+		catch(Exception e) {
 			ErrorHandling.saveErrLog("정규식 파싱 실패", "", e);
 		}
+
 		return pages;
 	}
 	
@@ -223,3 +197,9 @@ public class Preprocess implements DownloadMod {
 		downloader.close();
 	}
 }
+
+/*
+변경사항:
+DCL 싱글톤
+정규식 파싱 스트림 사용, 각 페이지별로 try...catch 사용하여 최대한 파싱
+*/
