73531267a0ddcecc129c1503efad389407613979
occidere <occidere@naver.com>
2017-12-19 16:44:45 +0900

    Refactoring ImageMerge.java Changed 1. FilenameFilter 구현을 람다식으로 변경 2. 확장자 체크 방식을 기존의 O(N^2)의 endswith체크에서 정규식 방식으로 변경

--- src/util/ImageMerge.java
+++ src/util/ImageMerge.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FilenameFilter;
 
 import javax.imageio.ImageIO;
 
@@ -13,11 +12,10 @@ import common.ErrorHandling;
 /**
  * 다운받은 만화를 하나의 세로로 긴 파일로 통합시킨 이미지를 생성하는 클래스
  * @author occidere
- *
  */
 public class ImageMerge{
 	private File root = null; //병합할 이미지들이 들어있는 폴더
-	private String imgExt[] = {"jpg", "jpeg", "bmp", "png", "gif"}; //지원되는 확장자
+	private String imgExt = ".+\\.(?i)(jpe?g|bmp|png|gif)$"; //확장자 정규식
 	
 	public ImageMerge() {
 		this(System.getProperty("user.dir"));
@@ -32,23 +30,16 @@ public class ImageMerge{
 		try {
 			System.out.print("이미지 병합중 ... ");
 			
-			int i, len, preHeight, width, height, maxWidth = -1, totalHeight = -1;
+			int i, len, preHeight, width, maxWidth = -1, totalHeight = -1;
 			
 			/* 폴더 내부의 모든 이미지(jpg, bmp등)를 얻음 */
-			File images[] = root.listFiles(new FilenameFilter() {
-				@Override
-				public boolean accept(File dir, String name) {
-					for(String ext : imgExt)
-						if(name.toLowerCase().endsWith(ext)) return true;
-					return false;
-				}
-			});
-			
+			File images[] = root.listFiles(f-> f.getName().matches(imgExt));
+
 			BufferedImage bi[] = new BufferedImage[len = images.length];
 			for(i=0;i<len;i++) {
 				bi[i] = ImageIO.read(images[i]);
 				maxWidth = Math.max(maxWidth, width = bi[i].getWidth()); //생성될 통합파일의 너비 계산
-				totalHeight+=(height = bi[i].getHeight()); //생성될 통합파일의 총 높이 계산
+				totalHeight += bi[i].getHeight(); //생성될 통합파일의 총 높이 계산
 			}
 			
 			/* 통합 이미지. 최대 너비 x 총 높이 */
@@ -57,7 +48,7 @@ public class ImageMerge{
 			graphics.setBackground(Color.BLACK); //여백은 검정으로 채움
 			
 			graphics.drawImage(bi[0], 0, 0, null); //처음엔 (0, 0)에 그리기
-			preHeight = bi[0].getHeight(); 
+			preHeight = bi[0].getHeight();
 			
 			for(i=1;i<len;i++) {
 				graphics.drawImage(bi[i], 0, preHeight, null);
@@ -74,3 +65,8 @@ public class ImageMerge{
 		System.out.println("성공");
 	}
 }
+/*
+변경사항
+FilenameFilter 구현을 람다식으로 변경
+확장자 체크 방식을 기존의 O(N^2)의 endswith체크에서 정규식 방식으로 변경
+*/
