b6874a28b18c528b86a1da3e988d0ee3ab1e3470
occidere <occidere@naver.com>
2018-06-13 01:22:05 +0900

    버전 0.5.1.1로 수정 & update date 2018.06.13으로 업데이

--- src/sys/SystemInfo.java
+++ src/sys/SystemInfo.java
@@ -58,8 +58,8 @@ public class SystemInfo {
 	private transient static final String LATEST_VERSION_URL = "https://github.com/occidere/MMDownloader/blob/master/VERSION_INFO";
 	
 	/* <수정 금지> 프로그램 정보 */
-	private static final String VERSION = "0.5.0.8"; //프로그램 버전
-	private static final String UPDATED_DATE = "2018.04.29"; //업데이트 날짜
+	private static final String VERSION = "0.5.1.1"; //프로그램 버전
+	private static final String UPDATED_DATE = "2018.06.13"; //업데이트 날짜
 	private static final String DEVELOPER = "제작자: occidere"; //제작자 정보
 	private static final String VERSION_INFO = String.format("현재버전: %s (%s)", VERSION, UPDATED_DATE);
 	
